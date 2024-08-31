8cc62a68cd95d1f998c9abc4450ecbcd9db75341
Brandon Plyler <brandonplyler1768@gmail.com>
2024-08-31 16:15:38 -0400

    Initial commit

---
+++ ChatGPTTest.py
@@ -0,0 +1,18 @@
+from openai import OpenAI
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+completion = client.chat.completions.create(
+    model="gpt-4",
+    messages=[
+        {
+            "role": "user",
+            "content": "Say this is a test",
+        },
+    ],
+)
+print(completion.choices[0].message.content)
