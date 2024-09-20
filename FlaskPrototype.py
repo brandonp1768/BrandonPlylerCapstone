a29318ef873ac29bc3ed8808f5189b44f67756a3
Brandon Plyler <brandonplyler1768@gmail.com>
2024-09-20 17:24:15 -0400

    Developed web app prototype

---
+++ FlaskPrototype.py
@@ -0,0 +1,48 @@
+from flask import Flask, render_template, request, redirect, url_for, flash
+from ChatClasses.QAAssistant import QAAssistant
+
+app = Flask(__name__)
+app.secret_key = 'G3n3r@t3d$ecreTKeY!XyZ12345'  # Move this to the environment variables
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+@app.route('/upload_file', methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        flash("No File Part", "Error")
+        return redirect(url_for('index'))
+    
+    file = request.files['file']
+
+    if file.filename == '':
+        flash("No File Selected", "Error")
+        return redirect(url_for('index'))
+    
+    # Save the file or process it as needed
+    file.save(f'uploads/{file.filename}')  
+
+    fileContent = read_file(file.filename)
+
+    Assistant = QAAssistant()
+
+    response = Assistant.EdgeCases(fileContent)
+
+    flash(f"{response}", "Success")
+    return redirect(url_for('index'))
+
+def read_file(fileName):
+    try:
+        with open(f'uploads/{fileName}', 'r') as file:
+            content = file.read()
+    except FileNotFoundError:
+        flash(f'The File At uploads/{fileName} Does Not Exit.')
+    except IOError:
+        flash('An IOError Occurred')
+
+    return content
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000) # look into the waitress package in python to get rid of development server
